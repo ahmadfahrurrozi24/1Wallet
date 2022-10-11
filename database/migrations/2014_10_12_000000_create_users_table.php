8ae0525b72567d0828f5ea2f4a8bb97f574975d2
jevvonn <darmawanb985@gmail.com>
2022-10-11 08:58:30 +0700

    🔨 : adding balance in user schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->decimal("balance" , 12 , 2);
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
