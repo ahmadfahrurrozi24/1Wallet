fe9670c1d7ad37b772f199ca706ebbd64ffe6186
jevvonn <darmawanb985@gmail.com>
2022-10-12 09:17:01 +0700

    🛠️ : adding & modify route & register function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,20 +14,7 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    public function index(){}
 
     /**
      * Store a newly created resource in storage.
@@ -33,9 +22,15 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        $data = $request->all();
+        $data["password"] = Hash::make($request->password);
+        $data["balance"] = $data["balance"] + 0.00;
+        unset($data["_token"]);
+
+        User::create($data);
+        return redirect()->to("login")->with("message" , "Register success, please login!");
     }
 
     /**
@@ -72,14 +67,18 @@ class UserController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
+    public function login(){
+        $data = [
+            "title" => "Login"
+        ];
+        return view("auth.login" , $data);
     }
+
+    public function register(){
+        $data = [
+            "title" => "Register"
+        ];
+        return view("auth.register" , $data);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [UserController::class , "login"]);
+Route::get('/register', [UserController::class , "register"]);
+
+Route::post('/login', [UserController::class , "login"]);
+Route::post('/register', [UserController::class , "store"]);
