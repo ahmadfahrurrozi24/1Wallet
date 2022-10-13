d772230a4469dec6db2b08d8f87df74f1a6a4a5f
jevvonn <darmawanb985@gmail.com>
2022-10-13 23:15:17 +0700

    🔨 : adding role id in user schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("role_id");
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->decimal("balance" , 12 , 2);
+            $table->decimal("balance", 12, 2);
             $table->rememberToken()->nullable();
             $table->timestamps();
         });
