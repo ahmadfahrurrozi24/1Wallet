d50a6d2c661614866713b6ebfa83ee3464553857
jevvonn <darmawanb985@gmail.com>
2022-10-14 17:49:43 +0700

    🛠️ : adding icon and foreignId type

--- database/migrations/2022_10_11_024633_create_categories_table.php
+++ database/migrations/2022_10_11_024633_create_categories_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("type_id");
             $table->string("name");
+            $table->string("icon");
             $table->timestamps();
         });
     }
