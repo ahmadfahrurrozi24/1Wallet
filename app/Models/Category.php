f34d51c2530ccce39978f33bfa4043cdd633cde2
jevvonn <darmawanb985@gmail.com>
2022-10-14 17:52:48 +0700

    🔨 : adding type relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,9 +9,15 @@ class Category extends Model
 {
     use HasFactory;
     protected $guarded = ["id"];
+    protected $with = ["type"];
 
-    public function record(){
-        return $this->hasMany(Record::class , "category_id");
+    public function record()
+    {
+        return $this->hasMany(Record::class, "category_id");
     }
 
+    public function type()
+    {
+        return $this->belongsTo(Type::class, "type_id");
+    }
 }
