8581c14790f57915ddd1eebf50d26c0d848417f4
jevvonn <darmawanb985@gmail.com>
2022-10-16 00:05:51 +0700

    ➕ : make helper class and register it to config

---
+++ app/Helper/Helper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helper;
+
+class Helper
+{
+  public static function balanceFormat($balance)
+  {
+    $balance = number_format($balance, 0, ',', '.');
+    return  "Rp. $balance";
+  }
+
+  public static function amountFormat($amount)
+  {
+    $amount = number_format($amount, 0, ',', '.');
+    if ($amount > 0) {
+      return "+ Rp. $amount";
+    } elseif ($amount < 0) {
+      $amount = abs($amount);
+      return "- Rp. $amount";
+    }
+  }
+}
