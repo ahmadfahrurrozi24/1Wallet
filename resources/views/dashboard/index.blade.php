862469fbbac067e17140f11b9148c8692badd9d1
jevvonn <darmawanb985@gmail.com>
2022-10-15 12:24:14 +0700

    🔨 : adding exp-inc total in week & month at view

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,4 +2,8 @@
 @section('content')
     <h1>Index Dashboard</h1>
     <h2>My balance : @currency(auth()->user()->balance) </h2>
+    <h2>My expense this week : @currency($recordTotal["totalExpenseWeek"])</h2>
+    <h2>My income this week : @currency($recordTotal["totalIncomeWeek"])</h2>
+    <h2>My expense this month : @currency($recordTotal["totalExpenseMonth"])</h2>
+    <h2>My income this month : @currency($recordTotal["totalIncomeMonth"])</h2>
 @endsection
