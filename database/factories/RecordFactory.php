b45e9bb33eeda95402acbf68ba481b9232a8caf2
jevvonn <darmawanb985@gmail.com>
2022-10-16 00:06:18 +0700

    🔧 : modif record factory curr & first balance

--- database/factories/RecordFactory.php
+++ database/factories/RecordFactory.php
@@ -16,10 +16,19 @@ class RecordFactory extends Factory
      */
     public function definition()
     {
+        $category_id = fake()->numberBetween(1, 2);
+        $amount = 0;
+
+        if ($category_id == 1) {
+            $amount = fake()->numberBetween(-100000, -20000);
+        } else {
+            $amount = fake()->numberBetween(20000, 100000);
+        }
+
         return [
             "user_id" => fake()->numberBetween(1, 11),
-            "category_id" => fake()->numberBetween(1, 2),
-            "amount" => fake()->numberBetween(20000, 100000)
+            "category_id" => $category_id,
+            "amount" => $amount
         ];
     }
 }
