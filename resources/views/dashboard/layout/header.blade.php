0d67965e4992c64a12f223b97497ae152f11849b
jevvonn <darmawanb985@gmail.com>
2022-10-16 22:11:50 +0700

    ➕ : making header and sidebar for dashboard view

---
+++ resources/views/dashboard/layout/header.blade.php
@@ -0,0 +1,10 @@
+<div class="header">
+  <div class="header-left">
+    <button class="hamburger-button">
+      <i class="bx bx-menu" ></i>
+    </button>
+  </div>
+  <div class="header-right">
+    <p>Hello, {{ auth()->user()->name }}</p>
+  </div>
+</div>
---
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -0,0 +1,49 @@
+<div class="sidebar">
+  <!-- Logo -->
+  <div class="logo_content">
+    <div class="logo">
+      <img src="img/logo.png" alt="" />
+      <button class="hamburger-button">
+        <i class="bx bx-menu sidebar-hamburger" ></i>
+      </button>
+    </div>
+  </div>
+  <!-- Navbar -->
+  <ul class="navList">
+    <li>
+      <a href="1-Wallet.html">
+        <i class="bx bxs-dashboard"></i>
+        <span class="name">Dashboard</span>
+      </a>
+      <!-- <span class="tooltip">Dashboard</span> -->
+    </li>
+    <li>
+      <a href="Histori.html">
+        <i class="bx bx-history"></i>
+        <span class="name">History</span>
+      </a>
+      <!-- <span class="tooltip">History</span> -->
+    </li>
+    <li>
+      <a href="">
+        <i class="bx bx-wallet"></i>
+        <span class="name">Add Record</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <i class="bx bx-line-chart"></i>
+        <span class="name">Insight</span>
+      </a>
+    </li>
+    <hr />
+    <li>
+      <form action="">
+        <button>
+          <i class="bx bx-log-out-circle"></i>
+          <span class="name">Logout</span>
+        </button>
+      </form>
+    </li>
+  </ul>
+</div>
