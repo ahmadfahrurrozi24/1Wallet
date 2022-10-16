9e3676378f842962305e3fd502784d3370890596
jevvonn <darmawanb985@gmail.com>
2022-10-16 22:10:19 +0700

    ➕ : making css & js dashbord view & add logo img

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function () {
+    sidebar.classList.toggle("active");
+};
+
+// document.querySelector(".wallet").onclick = function () {
+//     sidebar.classList.toggle("active");
+// };
