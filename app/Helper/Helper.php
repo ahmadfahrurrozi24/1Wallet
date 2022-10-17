fec2f10af9fdbbab93d27ec7ad3ebd8c211f3c09
jevvonn <darmawanb985@gmail.com>
2022-10-17 11:15:18 +0700

    🔧 : modifying balance & amount format

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -7,17 +7,17 @@ class Helper
   public static function balanceFormat($balance)
   {
     $balance = number_format($balance, 0, ',', '.');
-    return  "Rp. $balance";
+    return  "Rp $balance";
   }
 
   public static function amountFormat($amount)
   {
     $amount = number_format($amount, 0, ',', '.');
     if ($amount > 0) {
-      return "+ Rp. $amount";
+      return "+ Rp $amount";
     } elseif ($amount < 0) {
       $amount = abs($amount);
-      return "- Rp. $amount";
+      return "- Rp $amount";
     }
   }
 }
