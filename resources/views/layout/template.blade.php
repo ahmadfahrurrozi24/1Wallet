4daca705921b117012536b9442ba8b96e42ba554
jevvonn <darmawanb985@gmail.com>
2022-10-18 21:32:20 +0700

    adding tostfy js

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -6,11 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ $title }}</title>
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
   @yield('css')
 </head>
 <body>
   @yield('content')
+  
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
   @yield('js')
 </body>
 </html>
