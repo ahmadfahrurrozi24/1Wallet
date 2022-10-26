a31fe226af4eeeba7310246709de778ce766cabe
vidi123 <fadil.arofah79@gmail.com>
2022-10-26 08:49:26 +0700

    adding logo and show amount in history list

--- resources/views/dashboard/history.blade.php
+++ resources/views/dashboard/history.blade.php
@@ -82,9 +82,15 @@
             @foreach ($records as $record)
                 <div class="accordion">
                     <div class="contentbx">
-                        <div class="label">
-                            <h1>{{ Carbon::create($record->created_at)->day }}</h1>
-                            <span>{{ $record->category->name }}</span>
+                        <div class="label" data-typeAmount={{ $record->category->type->name }}>
+                            <div class="left-label">
+                              <i class="{{ $record->category->icon }}"></i>
+                              <span>{{ $record->category->name }}</span>
+                            </div>
+                            <div class="right-label">
+                              <span>@amount($record->amount)</span>
+                              <i class='bx bx-chevron-down icon-arrow'></i>
+                            </div>
                         </div>
                         <div class="history">
                             <div class="detail">
@@ -129,12 +135,71 @@
 @endsection
 @section('js')
     <script>
-        const accordion = document.getElementsByClassName('contentbx');
+      const accordion = document.querySelectorAll('.contentbx');
+      const row = document.querySelector('.bx-chevron-down');
+      const expense = document.querySelectorAll('.right-label span');
+      const label = document.querySelectorAll('.label');
 
-        for (i = 0; i < accordion.length; i++) {
-            accordion[i].addEventListener('click', function() {
-                this.classList.toggle('active')
-            })
+      for (let i = 0; i < label.length; i++) {
+        let labelType = label[i].getAttribute("data-typeAmount")
+        if(labelType == "EXPENSE"){
+          label[i].classList.add("expense-label")
         }
+      }
+
+      // expense[1].innerHTML
+      // if(expense.length.innerText = '-'){
+      //   for(let x = )
+      // }
+      
+      // const label = document.getElementsByClassName('label');
+      // if(expense.value == true ) {
+      //   label.style.backgroundColor = 'green';
+      // }
+      // if(expense.value = '-'){
+      //   label.style.backgroundColor = 'rgb(43 48 255)';
+      // }
+      
+      for (let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener("click" , function(){
+          accordion[i].classList.toggle("active")
+          let elmArrow = accordion[i].querySelector(".icon-arrow")
+
+          elmArrow.classList.replace('bx-chevron-down', 'bx-chevron-up');
+          if(accordion[i].classList.contains("active") == false){
+              elmArrow.classList.replace('bx-chevron-up', 'bx-chevron-down');
+          }
+        })
+      }
+      
+        
+      // accordion.forEach(elm => {
+      //     elm.addEventListener("click" , e => {
+      //       elm.classList.toggle("active")
+      //       let elmArrow = elm.querySelector(".icon-arrow")
+
+      //       elmArrow.classList.replace('bx-chevron-down', 'bx-chevron-up');
+      //       if(elm.classList.contains("active") == false){
+      //           elmArrow.classList.replace('bx-chevron-up', 'bx-chevron-down');
+      //       }
+      //     })
+      // })
+        // const accordion = document.getElementsByClassName('contentbx');
+        // const row = document.querySelector('.bx-chevron-down');
+        
+        // for (i = 0; i < accordion.length; i++) {
+        //   accordion[i].addEventListener('click', function() {
+        //     this.classList.toggle('active')
+
+        //     this.
+
+        //     row.classList.replace('bx-chevron-down', 'bx-chevron-up');
+        //     if(this.classList.contains('active') == false) {
+        //       row.classList.replace('bx-chevron-up', 'bx-chevron-down');
+        //     }
+        //     // label[0].style.transition = '0.4s 0.4s';
+        //       // label[0].style.transition = 'none';
+        //     })
+        // }
     </script>
 @endsection
