033c97717acb4646968dfff55dcb452ac47ce29d
jevvonn <darmawanb985@gmail.com>
2022-11-13 20:48:30 +0700

    adding js and hover on sidebar

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,19 @@
-let btn = document.querySelector("#btn");
+let btn = document.querySelectorAll(".hamburger-button");
 let sidebar = document.querySelector(".sidebar");
 
-btn.onclick = function () {
-    sidebar.classList.toggle("active");
-};
+btn.forEach((element) => {
+    element.onclick = function () {
+        sidebar.classList.toggle("active");
+    };
+});
+console.log();
+sidebar.addEventListener("mouseover", (e) => {
+    if (window.innerWidth > 518) sidebar.classList.add("active");
+});
+
+sidebar.addEventListener("mouseleave", (e) => {
+    if (window.innerWidth > 518) sidebar.classList.remove("active");
+});
 
 // document.querySelector(".wallet").onclick = function () {
 //     sidebar.classList.toggle("active");
--- resources/views/dashboard/layout/template.blade.php
+++ resources/views/dashboard/layout/template.blade.php
@@ -1,41 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $title }}</title>
-  @yield('css')
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset("css/style.css") }}">
-  <link rel="stylesheet" href="{{ asset("css/sidebar.css") }}">
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }}</title>
+    @yield('css')
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
 </head>
+
 <body>
-  @include('dashboard.layout.sidebar')
-  <div class="dashboard-content">
-    @include('dashboard.layout.header')
-    @yield('content')
-  </div>
+    @include('dashboard.layout.sidebar')
+    <div class="dashboard-content">
+        @include('dashboard.layout.header')
+        @yield('content')
+    </div>
 
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-
-  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script>
-    AOS.init();
-    
-    let btn = document.querySelectorAll(".hamburger-button");
-        let sidebar = document.querySelector(".sidebar");
-
-        btn.forEach(element => {
-            element.onclick = function () {
-                sidebar.classList.toggle("active");
-            };
-        });
-
-  </script>
-  @yield('js')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    {{-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> --}}
+    <script src="{{ asset('js/index.js') }}"></script>
+    @yield('js')
 </body>
+
 </html>
