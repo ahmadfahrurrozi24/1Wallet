a742fc3f7d079c3f4d200b70178ec5e86ddfcf90
DiandraRifqiM <luxamsky@gmail.com>
2022-11-21 11:51:23 +0700

    Admin and Category Create added

--- resources/views/dashboard/admin/category/index.blade.php
+++ resources/views/dashboard/admin/category/index.blade.php
@@ -1,10 +1,57 @@
 @extends('dashboard.layout.template')
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/category.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 @endsection
 @section('content')
     <div class="wrapper">
-        <h1>Index</h1>
+        <div class="create-title">
+            <a href="/dashboard/admin/category/create">
+                <h2>Create Category
+            </a>
+        </div>
+        <div class="table-wrapper">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Categories</th>
+                        {{-- <th>CAP</th>
+                        <th>INCH</th>
+                        <th>BOX TYPE</th> --}}
+                    </tr>
+                    <thead>
+                    <tbody>
+                        <tr>
+                            <td>Expense</td>
+                            <td>Fuel Cost</td>
+                            {{-- <td>9mm</td>
+                            <td>1/2"</td>
+                            <td>Kangal / Coil</td> --}}
+                        </tr>
+                        <tr>
+                            <td>Income</td>
+                            <td>Entertainment</td>
+                            {{-- <td>9mm</td>
+                            <td>1/2"</td>
+                            <td>Kangal / Coil</td> --}}
+                        </tr>
+                        <tr>
+                            <td>Income</td>
+                            <td>Clothes</td>
+                            {{-- <td>9mm</td>
+                            <td>1/2"</td>
+                            <td>Kangal / Coil</td> --}}
+                        </tr>
+                        <tr>
+                            <td>Expense</td>
+                            <td>Food & Baverage</td>
+                            {{-- <td>9mm</td>
+                            <td>1/2"</td>
+                            <td>Kangal / Coil</td> --}}
+                        </tr>
+                    </tbody>
+            </table>
+        </div>
     </div>
 @endsection
 @section('js')
