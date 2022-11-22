ea8e282aef1032a8efb284f0cfb6d0f802f07985
vidi123 <fadil.arofah79@gmail.com>
2022-11-22 22:30:42 +0700

    adding description and scrollbar style in landing page

--- resources/views/landingpage/landingPage.blade.php
+++ resources/views/landingpage/landingPage.blade.php
@@ -51,8 +51,8 @@
             <div class="Content-Transaction">
                 <img class="img-transaction" src="{{ asset('img/transaction.jpg') }}">
                 <div class="img-slogan">
-                    <h1>Easy way to</h1>
-                    <h1>Add transaction</h1>
+                    <h1>Easy way to Add transaction</h1>
+                    <p>It will take a few minutes to record your daily transactions. choose a category that is clear and tailored to your Expenses such as: Food, Shopping or Income: Salary, Gifts.</p>
                 </div>
             </div>
 
@@ -60,8 +60,8 @@
             <div class="Content-History">
                 <img class="img-History" src="{{ asset('img/history.jpg') }}">
                 <div class="img-slogan">
-                    <h1>Easy way to check</h1>
-                    <h1>Financial statement</h1>
+                    <h1>Easy way to check Financial statement</h1>
+                    <p>Your records are clearly attached to every report you report. Understand where your money is coming and going with easy-to-read graphs.</p>
                 </div>
             </div>
 
@@ -69,11 +69,14 @@
             <div class="Content-Insight">
                 <img class="img-Insight" src="{{ asset('img/dashboard.jpg') }}">
                 <div class="img-slogan">
-                    <h1>Easy way to check</h1>
-                    <h1>Income and expense</h1>
+                    <h1>Easy way to check Income and expense</h1>
+                    <p>it takes seconds to start efficient management performance indicator, are you ready to manage your money well?</p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="footer">
+        <p>Copyright &copy; 2022 Onewalletcompany All Rights Reserved</p>
     </div>
 
 </body>
